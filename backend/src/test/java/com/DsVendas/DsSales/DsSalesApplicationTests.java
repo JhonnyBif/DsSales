1103ce52e99fefd5611d5bd36458cb8c0abb829d
jhonny.mezzari <jhonnybif@unesc.net>
2023-07-06 16:07:16 -0300

    first commit

---
+++ backend/src/test/java/com/DsVendas/DsSales/DsSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DsVendas.DsSales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
